0bd3e34093de541e0d7a91e7d24dfa9ad652cf9d
unknown <phamgiang2811@gmail.com>
2019-05-18 15:40:21 +0700

    dom_event_html

---
+++ dom_events_practical/dom_events.js
@@ -0,0 +1,15 @@
+function Clicked(){
+    console.log("Clicked!!");
+    
+}
+function color(){
+    document.getElementById("myH1").style.color = "red";
+    document.getElementById("myH1").innerHTML = "Tadaaa. Chữ sau khi được thẩm mỹ!!";
+}
+function colorBox(){
+    document.getElementById("box").style.backgroundColor = "red";
+}
+function hideBox(){
+    document.getElementById("box1").style.width = 0;
+    document.getElementById("box1").style.height = 0;
+}
