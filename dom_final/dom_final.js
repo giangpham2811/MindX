3b24b1c041e38e655b938770b8f307cc6a372a66
unknown <phamgiang2811@gmail.com>
2019-05-18 16:30:26 +0700

    update dom_read

---
+++ dom_final/dom_final.js
@@ -0,0 +1,9 @@
+var song_container = document.getElementById("song_container");
+var songs = song_container.getElementsByClassName("song");
+
+// // select the element that will be replaced
+// var el = document.querySelector('i');
+// // <a href="/javascript/manipulation/creating-a-dom-element-51/">create a new element</a> that will take the place of "el"
+// var newEl = document.createElement('button');
+// // replace el with newEL
+// el.parentNode.replaceChild(newEl, el);
---
+++ dom_find_elements/find.js
@@ -0,0 +1,9 @@
+// var search = document.getElementById("btn_Search");
+//  var rect = document.getElementsByClassName("rect");
+// for (var i = 0 ; i<rect.length;i++){
+//     console.log(rect[i]);
+// }
+// var divs = document.getElementsByTagName("div");
+// for (var i = 0 ; i<divs.length;i++){
+//     console.log(divs[i]);
+// }
