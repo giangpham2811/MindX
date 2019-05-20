f12dfa9d2e65528c7e9fe55cbc79933e0c4daa59
Pham Minh Giang <phamgiang2811@gmail.com>
2019-05-20 21:01:23 +0700

    dom_final

--- dom_final/dom_final.js
+++ dom_final/dom_final.js
@@ -1,4 +1,9 @@
 var song_container = document.getElementById("song_container");
 var songs = song_container.getElementsByClassName("song");
-console.log(songs);
-songs.querySelection
+for (var i = 0 ; i < songs.length; i++){
+  console.log(songs[i]);
+  var title = songs[i].getElementsByClassName("title");
+  console.log("Title  : "  + title[0].innerText);
+  var artist = songs[i].getElementsByClassName("artist");
+  console.log("Artist : "  + artist[0].innerText);
+}
