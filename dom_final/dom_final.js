7532f0a7af26ea29dfca54df208feeb7a7d90d5a
Pham Minh Giang <phamgiang2811@gmail.com>
2019-05-22 15:34:51 +0700

    Le finnois tout le travail

--- dom_final/dom_final.js
+++ dom_final/dom_final.js
@@ -1,5 +1,6 @@
 var song_container = document.getElementById("song_container");
 var songs = song_container.getElementsByClassName("song");
+//get info in target
 for (var i = 0 ; i < songs.length; i++){
   console.log(songs[i]);
   var title = songs[i].getElementsByClassName("title");
@@ -7,3 +8,45 @@ for (var i = 0 ; i < songs.length; i++){
   var artist = songs[i].getElementsByClassName("artist");
   console.log("Artist : "  + artist[0].innerText);
 }
+//create button
+var btn_delete = document.getElementsByClassName("btn_delete");
+var btn_edit = document.getElementsByClassName("btn_edit");
+var btn_more = document.getElementsByClassName("btn_more");
+//delete
+for (let i = 0; i < btn_delete.length; i++) {
+  btn_delete[i].addEventListener('click',(e) => {
+    var btn = e.target;
+    var div = btn.parentNode;
+    div.remove();
+  });
+}
+//edit
+for (let i = 0; i < btn_edit.length; i++) {
+  btn_edit[i].addEventListener('click',(e) => {
+    var btn = e.target;
+    var div = btn.parentNode;
+    console.log("Song ID : " + div.getAttribute("song_id"));
+  });
+}
+//more
+for (let i = 0; i < btn_more.length; i++) {
+  btn_more[i].addEventListener('click',(e) => {
+    var btn = e.target;
+    var div = btn.parentNode;
+    console.log("Song ID : " + div.getAttribute("song_id"));
+    let title = songs[i].getElementsByClassName("title");
+    console.log("Song name : " +title[0].innerText);
+    let artist = songs[i].getElementsByClassName("artist");
+    console.log("Artist : " + artist[0].innerText);
+  });
+}
+//add
+var button = document.createElement("button");
+button. innerHTML = "Add";
+//
+var body = document.getElementsByTagName("body");
+body.appendChild(button);
+//
+button.addEventListener ("click", function() {
+  alert("This funtion is not finnish yet =))");
+});
